a6752fe4eff6d1efe4019ccd407d35724e76ff25
Serhii <silver383@gmail.com>
2021-03-06 13:33:22 +0200

    props

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = function () {
-    return <h1>Hello 2</h1>;
+const App = function (props) {
+    return (
+        <div>
+            <h1>Postman: </h1>
+            <ul>
+                <li>Name: {props.name}</li>
+                <li>Age: {props.age}</li>
+            </ul>
+        </div>
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App name="John" age="73" />, document.getElementById("root"));
