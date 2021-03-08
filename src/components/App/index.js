2e42b881996c90b3f7d5b7c5b7bbb433d39e53c3
Serhii <silver383@gmail.com>
2021-03-08 16:38:10 +0200

    localStorage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,9 +8,21 @@ class App extends Component {
             balance: 0,
         };
     }
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextState.balance < 5;
+    componentDidMount() {
+        let balance = JSON.parse(window.localStorage.getItem("balance"));
+        this.setState({
+            balance,
+        });
     }
+    STORE = () => {
+        window.localStorage.setItem(
+            "balance",
+            JSON.stringify(this.state.balance)
+        );
+    };
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return nextState.balance < 5;
+    // }
     onIncrease = () => {
         this.setState({
             balance: this.state.balance + 1,
@@ -27,6 +39,7 @@ class App extends Component {
                 <Balance balance={this.state.balance} />
                 <button onClick={this.onIncrease}>Add 1</button>
                 <button onClick={this.onDecrease}>Minus 1</button>
+                <button onClick={this.STORE}>STORE</button>
             </div>
         );
     }
