c3da49dd69ea1590f8f37a2e4780cf8793ceec84
Serhii <silver383@gmail.com>
2021-06-24 10:54:05 +0300

    you-dont-know-js

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -9,45 +9,16 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            balance: 0,
-            transactions: [
-                {
-                    label: "void",
-                    value: 0,
-                    id: 0,
-                },
-            ],
+            value: 0,
         };
     }
 
-    componentDidMount() {
-        let balance = JSON.parse(window.localStorage.getItem("balance"));
-        this.setState({
-            balance,
-        });
-    }
+    componentDidMount() {}
 
-    STORE = () => {
-        window.localStorage.setItem(
-            "balance",
-            JSON.stringify(this.state.balance)
-        );
-    };
-
-    onChange = (value) => {
-        this.setState((state) => ({
-            balance: state.balance + Number(value),
-            transactions: [{ value, label: "change" }, ...state.transactions],
-        }));
-    };
     render() {
         return (
             <div>
-                <Balance balance={this.state.balance} />
-                <Form onChange={this.onChange} />
-                <button onClick={this.STORE}>STORE</button>
-
-                <Transactions transactions={this.state.transactions} />
+                <h1>JS</h1>
             </div>
         );
     }
